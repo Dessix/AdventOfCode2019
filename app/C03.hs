a9653c80f5d6a0fce952a12ae81dc96a9b4cc8f7
Zoey <Dessix@Dessix.net>
2019-12-22 19:10:49 -0800

    Added a file for day 3 and a dependency on Data.Vector

---
+++ app/C03.hs
@@ -0,0 +1,22 @@
+module C03 where
+
+import Control.Exception
+import Control.Lens
+import Control.Monad
+import Control.Monad.ST
+
+import Data.Array.IArray
+import Data.Array.MArray
+import qualified Data.Array.ST
+import Data.Function
+import Data.Functor
+import Data.List
+import Data.Ratio
+import Data.Vector
+
+import Debug.Trace
+
+import Text.Printf
+
+import Utils
+
--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ import Lib
 import Utils
 import C01
 import C02
+import C03
 
 main :: IO ()
 main = someFunc
