e08ec411e15f2c025a81994ce68cbee5011faacc
Zoey <Dessix@Dessix.net>
2019-12-28 17:29:09 -0800

    Solved day 3 part 1

--- app/C03.hs
+++ app/C03.hs
@@ -16,6 +16,8 @@ import qualified Data.Vector
 import qualified Data.Text as T
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 import Debug.Trace
 
@@ -59,13 +61,44 @@ parsePuzzleLines plines =
     in
     plines & map entriesMapper
 
+wirePositions :: (Int, Int) -> WireSequence -> [(Int, Int)]
+wirePositions start (WireSequence nodes) =
+    let genPositionsForDirection (startX, startY) dir len =
+            if len == 0 then []
+            else
+                let (dx, dy) = case dir of
+                        DUp -> (0, -1)
+                        DDown -> (0, 1)
+                        DLeft -> (-1, 0)
+                        DRight -> (1, 0)
+                in
+                let pos' = (startX + dx, startY + dy) in
+                pos' : genPositionsForDirection pos' dir (pred len)
+    in
+    case nodes of
+        (WireNode (dir, len)) : remaining ->
+            let nextPositions = genPositionsForDirection start dir len in
+                nextPositions ++ (wirePositions (last (start : nextPositions)) (WireSequence remaining))
+        [] -> []
+
 findWireIntersections :: [WireSequence] -> [(Int, Int)]
-findWireIntersections =
-    undefined
+findWireIntersections sequences =
+    let origin = (0, 0) in
+    let wirePositions' = wirePositions origin in
+    let buildPositionSet sequence =
+            Set.fromList $ wirePositions' sequence
+    in
+    let posSequences = map buildPositionSet sequences in
+    let intersectionFolder a b =
+            Set.intersection a b
+    in
+    Set.toList $ foldr1 intersectionFolder posSequences
+
+manhattanDistance :: (Int, Int) -> (Int, Int) -> Word
+manhattanDistance (x, y) (x', y') = fromIntegral ((abs (x' - x)) + (abs (y' - y)))
 
 solvePuzzleFromLines :: IO ()
-solvePuzzleFromLines =
-    do
+solvePuzzleFromLines = do
     l <- Utils.getLinesUntilBlank
-    let solution = findWireIntersections $ parsePuzzleLines l in
-        putStrLn $ printf "Solved! %s" $ show solution
+    let wireIntersections = findWireIntersections $ parsePuzzleLines l in
+        putStrLn $ printf "Solved! %d" $ minimum $ map (manhattanDistance (0, 0)) wireIntersections
